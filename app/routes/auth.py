d5e05a6dc30604c7d0c5e33c9aa7e51068ecc535
ArViLi06 <utofery@gmail.com>
2026-06-27 21:27:50 +0700

    feat: implement sign-up functionality and enhance login page layout

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -5,3 +5,7 @@ auth_bp = Blueprint('auth', __name__)
 @auth_bp.get('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth_bp.get('/sign-up')
+def sign_up():
+    return render_template('auth/sign-up.html')
